c641329065a01ddad5742217abbf0ff7e4e0f152
♡⁠(⁠⁠ ⁠ਊ⁠ ⁠⁠)⁠♡ <229762773+elaizaespartinez@users.noreply.github.com>
2026-04-26 00:38:25 +0800

    GUI_Form1LogIn

---
+++ Final Project G3/EveryJuanCount/Form1.cs
@@ -0,0 +1,83 @@
+namespace EveryJuanCount
+{
+    public partial class LogInForm1 : Form
+    {
+        public LogInForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EJC_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UsernameTB1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt1ShowPass_Click(object sender, EventArgs e)
+        {
+            if (txtB2Password.PasswordChar == '*')
+            {
+                bt2HidePass.BringToFront();
+                txtB2Password.PasswordChar = '\0';
+            }
+        }
+
+        private void bt2HidePass_Click(object sender, EventArgs e)
+        {
+            if (txtB2Password.PasswordChar == '\0')
+            {
+                bt1ShowPass.BringToFront();
+                txtB2Password.PasswordChar = '*';
+            }
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtB2Password_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt3LogIn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
